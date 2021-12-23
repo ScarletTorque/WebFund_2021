e4d8f61ae8246b8b5f59d228358b1e923ecd1bd3
ScarletTorque <michaelmunson151@gmail.com>
2021-12-22 21:32:00 -0600

    likes

---
+++ LikesI/script.js
@@ -0,0 +1,10 @@
+var num = 3;
+
+var countElement = document.querySelector("#count")
+
+
+function like() {
+    num++;
+    countElement.innerText =num + "likes";
+    console.log(count);
+}
---
+++ likesII/script.js
@@ -0,0 +1,32 @@
+var num1 = 9;
+
+var countElement1 = document.querySelector("#count1")
+
+
+function like1() {
+    num1++;
+    countElement1.innerText =num1 + "likes";
+    console.log(count1);
+}
+
+var num2 = 12;
+
+var countElement2 = document.querySelector("#count2")
+
+
+function like2() {
+    num2++;
+    countElement2.innerText =num2 + "likes";
+    console.log(count2);
+}
+
+var num3 = 9;
+
+var countElement3 = document.querySelector("#count3")
+
+
+function like3() {
+    num3++;
+    countElement3.innerText =num3 + "likes";
+    console.log(count3);
+}
